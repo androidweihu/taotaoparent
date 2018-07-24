c4ecbd865b1978599aca8ab72a7c813e25cbcea2
weihu <weihu@jd.com>
2018-07-25 00:24:24 +0800

    jsonp 把json数据封装到js中，成为js的一个片断

---
+++ taotao-rest/src/main/java/com/taotao/rest/pojo/ItemCatResult.java
@@ -0,0 +1,20 @@
+package com.taotao.rest.pojo;
+
+import java.util.List;
+
+/**
+ * @author weihu
+ * @date 2018/7/24/024 23:15
+ * @desc
+ */
+public class ItemCatResult {
+    private List data;
+
+    public List getData() {
+        return data;
+    }
+
+    public void setData(List data) {
+        this.data = data;
+    }
+}
---
+++ taotao-rest/src/main/java/com/taotao/rest/service/ItemCatService.java
@@ -0,0 +1,13 @@
+package com.taotao.rest.service;
+
+import com.taotao.rest.pojo.ItemCatResult;
+
+/**
+ * @author weihu
+ * @date 2018/7/24/024 23:36
+ * @desc
+ */
+public interface ItemCatService {
+
+    ItemCatResult getItemCatList();
+}
